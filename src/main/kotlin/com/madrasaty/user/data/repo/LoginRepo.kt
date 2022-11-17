8f513a236be98cef881633d8f0736103f358c9f9
Hassanein Altememy <hassanein.altememy@rabobank.nl>
2022-11-17 17:12:31 +0100

    Remove test package name and fix structure, change repo's to JpaRepository

--- src/main/kotlin/com/madrasaty/user/data/repo/LoginRepo.kt
+++ src/main/kotlin/com/madrasaty/user/data/repo/LoginRepo.kt
@@ -1,8 +1,9 @@
 package com.madrasaty.user.data.repo
 
 import com.madrasaty.user.data.entity.Login
+import io.micronaut.data.jpa.repository.JpaRepository
 import io.micronaut.data.repository.kotlin.CoroutineCrudRepository
 import java.util.*
 
-interface LoginRepo: CoroutineCrudRepository<Login, UUID> {
+interface LoginRepo: JpaRepository<Login, UUID> {
 }
